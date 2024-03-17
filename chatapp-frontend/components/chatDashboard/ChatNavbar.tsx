00a031fea98816fecbc097a32a16923f587eb18f
vivek6201 <gvicky4567888@gmail.com>
2024-03-17 16:58:11 +0530

    added responsiveness to the app

--- chatapp-frontend/components/chatDashboard/ChatNavbar.tsx
+++ chatapp-frontend/components/chatDashboard/ChatNavbar.tsx
@@ -5,11 +5,23 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import { selectedUser } from "@/store/atoms/userAtom";
 import { useSocket } from "@/Contexts/SocketProvider";
 import { typingAtom } from "@/store/atoms/chatAtom";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import ChatList from "./ChatList";
+import { useSession } from "next-auth/react";
+import { Menu } from "lucide-react";
 
 export default function ChatNavbar() {
   const user = useRecoilValue(selectedUser);
   const [typing, setTyping] = useRecoilState(typingAtom);
   const { socket } = useSocket();
+  const { data: session }: any = useSession();
 
   useEffect(() => {
     if (socket) {
@@ -34,6 +46,15 @@ export default function ChatNavbar() {
   return (
     <div className="flex items-center justify-between p-4 border-b border-gray-200 dark:border-gray-800">
       <div className="flex items-center space-x-4">
+        <Sheet>
+          <SheetTrigger>
+            <Menu className="block md:hidden" />
+          </SheetTrigger>
+          <SheetContent side={"left"}>
+            <ChatList user={session?.user} />
+          </SheetContent>
+        </Sheet>
+
         <Avatar>
           <AvatarFallback className="uppercase">
             {user.firstName.split("")[0]}
@@ -41,14 +62,14 @@ export default function ChatNavbar() {
           </AvatarFallback>
         </Avatar>
         <div className="flex items-center space-x-2">
-          <div className="space-y-1.5">
+          <div className="space-y-1.5 transition-all duration-300">
             <div className="font-semibold capitalize">
               {user.firstName} {user.lastName}
             </div>
             <div className="text-xs text-gray-500 dark:text-gray-400">
               {typing.from === user.id && typing.status
                 ? "Typing..."
-                : "Online"}
+                : ""}
             </div>
           </div>
         </div>
--- chatapp-frontend/components/chatDashboard/LandingPage.tsx
+++ chatapp-frontend/components/chatDashboard/LandingPage.tsx
@@ -2,17 +2,29 @@
 import { userInfo } from "@/store/atoms/userAtom";
 import React from "react";
 import { useRecoilValue } from "recoil";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Menu } from "lucide-react";
+import ChatList from "./ChatList";
 
-export default function LandingPage() {
+export default function LandingPage({ data }: any) {
   const user = useRecoilValue(userInfo);
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <p>
-        Welcome{" "}
-        <span className="font-bold text-xl capitalize">{user.firstName}</span>{" "}
-        to Apna Chat App
-      </p>
+    <div className="grid grid-rows-[70px_1fr] md:grid-rows-1 place-items-center h-full">
+      <div className="flex items-center justify-between p-4 border-b border-gray-200 dark:border-gray-800 md:hidden w-full">
+        <div className="flex items-center space-x-4">
+          <Sheet>
+            <SheetTrigger>
+              <Menu className="block md:hidden" />
+            </SheetTrigger>
+            <SheetContent side={"left"}>
+              <ChatList user={data} />
+            </SheetContent>
+          </Sheet>
+          <p className="font-bold uppercase text-center">APNA CHAT</p>
+        </div>
+      </div>
+      <p>Welcome <span className="font-bold text-xl capitalize">{user.firstName}</span> to Apna chat app</p>
     </div>
   );
 }
